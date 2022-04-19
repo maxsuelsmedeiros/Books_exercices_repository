136e2098e0733990bc8e0aa0c40216013a53a772
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2022-04-18 21:00:39 -0300

    removing repetitive codes

--- Exercicios/Capitulo_1/Exercicio_1_2.py
+++
@@ -1,9 +0,0 @@
-def main():
-    n=1000000
-    print("Hello, World!\n")
-    print((10000/2562)*3.6)
-    print(10/0.707)
-    print(6,21/0.707)
-
-if __name__ == "__main__":
-    main()
--- Exercicios/Capitulo_9/Questão_2.py
+++
@@ -1,8 +0,0 @@
-def main():
-    def has_no_e(word):
-        return True if word.find("e")==-1 else False
-    print(has_no_e("atlanta"))
-
-
-if __name__=='__main__':
-    main()
