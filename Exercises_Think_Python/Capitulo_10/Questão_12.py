a2ef3f8bd95e76c66d17e54296ffbdea0b1ce8d9
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2022-05-07 11:07:57 -0300

    adding question 12 from chapter 10, but sincerely I don't think it is correct, the code takes too long to finish

---
+++ Exercises_Think_Python/Capitulo_10/Questão_12.py
@@ -0,0 +1,65 @@
+import bisect as bi
+import time
+def main():
+    bag=open("Exercises_Think_Python/words.txt")
+    def remove_useless_simbols(bag):
+        new_bag=[]
+        for word in bag:
+            word=word.strip("'")
+            new_bag.append(word.strip("\n"))
+        return new_bag
+    bag_of_words=remove_useless_simbols(bag)
+    #Function that join words:
+    def join_words(word1,word2):
+        new_word=""
+        word1=list(word1)
+        word2=list(word2)
+        count=0
+        if len(word1)==len(word2):
+            for letter in word1:
+                new_word = new_word + word1[count] + word2[count]
+                count+=1
+        elif len(word1)>len(word2):
+            for letter in word2:
+                new_word = new_word + word1[count] + word2[count]
+                count+=1
+                for letter in range(len(word2),len(word1)):
+                    new_word= new_word + word1[letter]
+        else:
+            for letter in word1:
+                new_word = new_word + word1[count] + word2[count]
+                count+=1
+            for letter in range(len(word1),len(word2)):
+                new_word= new_word + word2[letter]
+        return new_word
+    #Function that verify if the list is not empity
+    def is_empty(t):
+        if t == []:
+            return True
+        else:
+            return False
+    #Function that verify the existence of a word in a list faster then the IN operator
+    def in_bisect(bag,word):
+        position=bi.bisect_left(bag,word)
+        if bag[position]==word:
+            return position
+        else:
+            return "None"
+    #This function joins 2 words and verify if the word exists
+    def new_words(bag):
+        new_word=""
+        list_new_word=[]
+    #Running through the list of words and then doing the same operation to verify if the words together make new valid words
+        for i in bag:
+            for j in bag:
+                if j!=i:
+                    new_word=join_words(i,j)
+                    if new_word in bag and new_word not in list_new_word and is_empty(list_new_word) is False:
+                        print("HEHE")
+                        list_new_word.append(new_word)
+        return list_new_word
+    print(new_words(bag_of_words))
+    
+
+if __name__=='__main__':
+    main()
