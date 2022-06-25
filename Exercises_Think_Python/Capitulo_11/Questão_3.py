434d5b0290af63a73e65352cb80361ede3e764ce
maxsuelsmedeiros <maxsuelmedeiros@hotmail.com>
2022-06-25 15:24:54 -0300

    could not understand very well how to implement a memo in this dictionary of the ackermann algorithm

---
+++ Exercises_Think_Python/Capitulo_11/Questão_3.py
@@ -0,0 +1,12 @@
+def main():
+    def ackermann(m, n):
+        if m == 0:
+            return n+1
+        if n == 0:
+            return ackermann(m-1, 1)
+        return ackermann(m-1, ackermann(m, n-1))
+
+    print (ackermann(3, 4))
+
+if __name__=='__main__':
+    main()
